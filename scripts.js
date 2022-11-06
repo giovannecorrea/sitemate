bed73b9b6c17a9901fae918b813c8174bd072238
Giovanne Corrêa <giovannecorrea@gmail.com>
2022-11-06 11:16:11 -0300

    custom messages

--- scripts.js
+++ scripts.js
@@ -1,16 +1,46 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-    
+
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var yesBtn = document.getElementById("yesBtn");
+
+// Get the button that opens the modal
+var cancelBtn = document.getElementById("cancelBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+function openModal(btn) {
   document.getElementById("confirmation_message").innerText = btn.dataset.confirmation_message;
+  document.getElementById("yesBtn").value = btn.dataset.yes_message;
+  document.getElementById("cancelBtn").value = btn.dataset.cancel_message;
+  
   modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+yesBtn.onclick = function() {
+    const msg = document.createElement("p");
+    msg.innerText = document.getElementById("yesBtn").value;
+    document.body.appendChild(msg);
+    document.getElementById("yesBtn").value = "";
+    document.getElementById("cancelBtn").value = "";
+    modal.style.display = "none";
+}
+
+cancelBtn.onclick = function() {
+    const msg = document.createElement("p");
+    msg.innerText = document.getElementById("cancelBtn").value;
+    document.body.appendChild(msg);
+    document.getElementById("yesBtn").value = "";
+    document.getElementById("cancelBtn").value = "";
+    modal.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
